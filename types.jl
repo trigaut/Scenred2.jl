4a90980169cabe7a2350cd73f18f8c0d4f2a49ed
rigaut <tristan.rigaut@gmail.com>
2017-07-11 17:20:06 +0200

    add writedlm

--- types.jl
+++ types.jl
@@ -18,9 +18,9 @@ type Scenred2Scenario
 end
 
 type Scenred2Fan
-    stages::Int
-    n_random_variables::Int
-    n_scenarios::Int
+    TIME::Int
+    SCEN::Int
+    RANDOM::Int
     scenarios::Vector{Scenred2Scenario}
 end
 
@@ -35,6 +35,27 @@ end
 function Scenred2Prms(; construction_method::Int = 2, reduction_method = 1, 
                         order = 1, scaling = 0, red_percentage = 0.1)
     Scenred2Prms(construction_method, reduction_method, order, scaling, red_percentage)
+end
+
+function obj_to_dict!(d::Dict, obj; first_field::Int=1, last_field::Int=0)
+    for i in fieldnames(prms)[first_field:end-last_field]
+       d[i] = eval(quote prms.$i end)
+       end
+    end
+end
+
+function Base.writedlm(prms::Scenred2Prms) 
+    d = Dict()
+    obj_to_dict!(d, prms)
+    writedlm("scenred2Opt.opt", d)
+end
+
+function Base.writedlm(prms::Scenred2Fan)
+    d = Dict("TYPE" => "FAN")
+    obj_tp_dict!(d, last_field=1)
+    d["DATA"] = ""
+    
+    d["END"] = ""
 end
 
 function Scenred2Tree(f::Scenred2Fan, prms::Scenred2Prms)
